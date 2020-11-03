7691b43f9fada049920531050f81e6e7b9a71fd1
GustavoCesarSantos <gustavocs789@gmail.com>
2020-11-03 18:01:34 -0300

    Criado classe Fornecedor, adicionado endpoint post para cadastro de fornecedores, adicionado metodo inserir que faz a funcao da camada Dao do cadastro do fornecedor

--- src/rotas/fornecedores/index.js
+++ src/rotas/fornecedores/index.js
@@ -1,9 +1,18 @@
 const app = require('express').Router();
+
+const Fornecedor = require('./Fornecedor');
 const table = require('./table');
 
 app.get('/', async (req, res) => {
   const result = await table.listar();
   res.json(result);
+})
+
+app.post('/', async (req, res) => {
+  const fornecedorInfos = req.body;
+  const fornecedor = new Fornecedor(fornecedorInfos);
+  await fornecedor.criar();
+  res.json({ "msg": "OK" })
 })
 
 module.exports = app
--- src/rotas/fornecedores/table.js
+++ src/rotas/fornecedores/table.js
@@ -3,5 +3,9 @@ const model = require('./model');
 module.exports = {
   listar() {
     return model.findAll();
+  },
+
+  inserir(dados) {
+    return model.create(dados);
   }
 }
