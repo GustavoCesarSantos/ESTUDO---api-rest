c13da105a518ce37e3c529bdc5ba71cd401d72ff
GustavoCesarSantos <gustavocs789@gmail.com>
2020-11-08 01:21:25 -0300

    Criado serializador que formatar dados em json usando o cabecalho Content-Type, criado classe de erro de valor nao suportado e adicionado verificacao de tal erro e atribuido o status 406 a esse erro, criado middleware que verifica o tipo do aceito pelo cliente.

--- index.js
+++ index.js
@@ -3,11 +3,26 @@ const express = require('express');
 
 const CampoInvalido = require('./src/erros/CampoInvalido');
 const DadosNaoFornecidos = require('./src/erros/DadosNaoFornecidos');
+const FormatosACeitos = require('./src/Serializador').formatosAceitos;
 const NaoEncontrado = require('./src/erros/NaoEncontrado');
+const ValorNaoSuportado = require('./src/erros/ValorNaoSuportado');
 const rotas = require('./src/rotas/fornecedores/index');
+const { formatosAceitos } = require('./src/Serializador');
 
 const app = express();
 app.use(express.json());
+
+app.use((req, res, next) => {
+  let formatoRequisitado = req.headers['accept'];
+  if(formatoRequisitado === '*/*')
+    formatoRequisitado = 'application/json';
+
+  if(!formatosAceitos.includes(formatoRequisitado))
+    return res.status(406).end();
+
+  res.setHeader('Content-Type', formatoRequisitado);
+  next();
+})
 
 app.use('/api/fornecedores', rotas);
 app.use((error, req, res, next) => {
@@ -18,6 +33,9 @@ app.use((error, req, res, next) => {
   
   if(error instanceof CampoInvalido || error instanceof DadosNaoFornecidos) 
     status = 400;
+
+  if(error instanceof ValorNaoSuportado)
+    status = 406;
     
   res.status(status).json({ "error": error.message, "id": error.idError });
 })
